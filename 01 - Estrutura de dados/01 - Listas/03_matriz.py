87be469acad9d51390b916a59a44a14560163782
Thiago Macedo <111608892+thiagomacedoabdon@users.noreply.github.com>
2023-08-31 16:04:56 -0300

    Update 03_matriz.py

--- 01 - Estrutura de dados/01 - Listas/03_matriz.py
+++ 01 - Estrutura de dados/01 - Listas/03_matriz.py
@@ -5,6 +5,6 @@ matriz = [
 ]
 
 print(matriz[0])  # [1, "a", 2]
-print(matriz[0][0])  # 1 //Primeiro elemento da primeira linha da matriz
-print(matriz[0][-1])  # 2 //Na primeira linha com o ultimo elemento da linha (no caso que tb é o primeiro elemento de tras pra frente)
-print(matriz[-1][-1])  # "c" //Na ultima linha com o ultimo item da matriz
+print(matriz[0][0])  # 1 Primeiro elemento da primeira linha da matriz
+print(matriz[0][-1])  # 2 Na primeira linha com o ultimo elemento da linha (no caso que tb é o primeiro elemento de tras pra frente)
+print(matriz[-1][-1])  # "c" Na ultima linha com o ultimo item da matriz
